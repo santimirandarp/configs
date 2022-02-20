2ffd1808fcd652f3f5b970a72fb64d014d47c4a3
santi <santimir@pm.me>
2022-02-20 14:00:03 +0000

    synced

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -76,8 +76,8 @@ modkey = "Mod4"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
-    awful.layout.suit.floating,
     awful.layout.suit.tile,
+    awful.layout.suit.floating,
     awful.layout.suit.tile.bottom,
     awful.layout.suit.spiral,
     awful.layout.suit.spiral.dwindle,
